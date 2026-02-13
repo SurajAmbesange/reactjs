a109b27bb827754230bef332b668c1a977374c00
Suraj Ambesange <surajambesange4474@gmail.com>
2026-02-13 19:10:11 +0530

    Edited & added files

--- razorpay-clone/src/components/Navbar.jsx
+++ razorpay-clone/src/components/Navbar.jsx
@@ -4,23 +4,24 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="sticky top-0 z-50 py-3 border-b border-neutral-700">
-      <div className="container px-4 mx-auto text-sm">
-        <div className="flex justify-center items-center gap-2">
-          <img className="w-50 h-15 mr-2" src={razor} alt="" />  
-        
-          <ul className="flex space-x-600 justify-center gap-1">
-            <li>
-              <a href="" className="text-gray-300">Home</a>
-              <a href="" className="text-gray-300">About</a>
-              <a href="" className="text-gray-300">Services</a>
-              <a href="" className="text-gray-300">Contact</a>
-            </li>
+    <nav className="bg-dark-blue px-2 py-4 border-b border-gray-600 w-full">
+      <div className="flex justify-between items-center pl-4 pr-4">
+        <a href="#"><img src={razor} alt="Razorpay Logo" width="150px" height="100px" /></a>
+          <ul className="flex space-x-4">
+              <li className="md:flex text-white font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Home</a></li>
+              <li className="md:flex text-white font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">About</a></li>
+              <li className="md:flex text-white font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Services</a></li>
+              <li className="md:flex text-white font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Contact</a></li>
+           
           </ul>
-          
-        </div>
-      </div>    
+          <a href="#">
+            <button className="bg-blue-500 hover:bg-blue-300 font-bold text-white px-2 py-1 rounded">Know More</button>
+          </a>
+      </div>
     </nav>
+
+    //Hero Section
+    
   );
 };
 
