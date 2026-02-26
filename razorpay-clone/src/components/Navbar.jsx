304599bb610a97bda4e31ede8c035932e9faf677
Suraj Ambesange <surajambesange4474@gmail.com>
2026-02-26 19:47:16 +0530

    Edited Navbar

--- razorpay-clone/src/components/Navbar.jsx
+++ razorpay-clone/src/components/Navbar.jsx
@@ -5,24 +5,22 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="py-4 border-b border-gray-600 w-full">
-      <div className="flex justify-between items-center mx-auto px-20">
+    <nav className="bg-blue-950">
+      <div className="flex justify-between items-center mx-auto w-[1080px]">
         <a href="#"><img src={razor} alt="Razorpay Logo" width="150px" height="100px" /></a>
           <ul className="flex space-x-4 hidden md:flex">
-              <li className="relative text-gray-700 font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Payments</a></li>
-              <li className="relative text-gray-700 font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Banking+</a></li>
-              <li className="relative text-gray-700 font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Payroll</a></li>
-              <li className="relative text-gray-700 font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Engage</a></li>
-              <li className="relative text-gray-700 font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Partners</a></li>
-              <li className="relative text-gray-700 font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Resources</a></li>
-              <li className="relative text-gray-700 font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Pricing</a></li>
+              <li className="relative text-white font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Payments</a></li>
+              <li className="relative text-white font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Banking+</a></li>
+              <li className="relative text-white font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Payroll</a></li>
+              <li className="relative text-white font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Engage</a></li>
+              <li className="relative text-white font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Partners</a></li>
+              <li className="relative text-white font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Resources</a></li>
+              <li className="relative text-white font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Pricing</a></li>
           </ul>
-          <div className="flex flex-col justify-center items-start px-4">
-          <a href="#">
-            <img src={India} alt="" srcset="" />
-            <button className="bg-transparent hover:bg-gray-100  font-bold text-blue-700 font-bold border border-blue-800 px-2 py-1 rounded">Login</button>
-            <button className="bg-blue-600 mx-4 lg:mx-6 hover:bg-blue-800 font-bold text-white font-bold border border-blue-800 px-2 py-1 rounded">Sign Up</button>
-          </a>
+          <div className="flex space-x-6">
+              <img src={India} alt="" srcset="" width="48px" height="20px" />
+            <button className="font-bold text-blue-700 border border-blue-800 px-5 py-3 rounded">Login</button>
+            <button className="bg-blue-600 mx-4 lg:mx-6 hover:bg-blue-800 font-bold text-white border border-blue-800 px-2 py-1 rounded">Sign Up</button>
           </div>
       </div>  
     </nav>
