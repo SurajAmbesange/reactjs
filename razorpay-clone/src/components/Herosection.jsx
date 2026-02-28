581dd0aa8d2de2251a1f5b7f596584d72954f113
Suraj Ambesange <surajambesange4474@gmail.com>
2026-02-28 21:45:34 +0530

    Created Hero Section

--- razorpay-clone/src/components/Herosection.jsx
+++ razorpay-clone/src/components/Herosection.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import razorbg from "../assets/razorbg.avif"    
 const Herosection = () => {
   return (
-    <div className="text-lg font-bold p-5">Herosection</div>
+    <div className="w-10/12 max-w-[1080px] flex flex-row justify-between items-center mx-auto">
+      <div className="space-y-5">
+        <h1 className="font-poppins font-bold text-[40px] leading-1.2 text-blue-950">Effortless Banking <br /><span className="text-black">for India's boldest disruptors</span></h1>
+        <p className="text-gray-700 opacity-80 text-[16px] leading-[1.2] ">Powerful Automation | Smart Dashboard | Integrated Access</p>
+        <button className="bg-blue-600 hover:bg-blue-900 transition-all duration-200 text-white px-[18px] py-[10px] rounded-md font-bold ">Sign Up Now</button>
+        <button className="px-6 text-blue-600 hover:underline transition-all duration-500 font-bold">Know More</button>
+      </div>
+      <div>
+        <img src={razorbg} alt="" srcset="" width="100%" height="100%"  className='max-w-[680px]'/>
+      </div>
+    </div>
   )
 }
 
-export default Herosection
+export default Herosection 
--- razorpay-clone/src/components/Navbar.jsx
+++ razorpay-clone/src/components/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
 
   return (
     <nav className="bg-blue-950">
-      <div className="flex justify-between items-center mx-auto w-[1080px]">
-        <a href="#"><img src={razor} alt="Razorpay Logo" width="150px" height="100px" /></a>
+      <div className="flex justify-between items-center px-4 py-3 mx-auto w-[1080px]">
+        <a href="#"><img src={razor} alt="Razorpay Logo" width="150px" /></a>
           <ul className="flex space-x-4 hidden md:flex">
               <li className="relative text-white font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Payments</a></li>
               <li className="relative text-white font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Banking+</a></li>
@@ -17,16 +17,13 @@ const Navbar = () => {
               <li className="relative text-white font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Resources</a></li>
               <li className="relative text-white font-poppins font-bold hover:text-blue-500 cursor-pointer transition-all duration-200"><a href="#">Pricing</a></li>
           </ul>
-          <div className="flex space-x-6">
+          <div className="flex space-x-6 items-center">
               <img src={India} alt="" srcset="" width="48px" height="20px" />
-            <button className="font-bold text-blue-700 border border-blue-800 px-5 py-3 rounded">Login</button>
-            <button className="bg-blue-600 mx-4 lg:mx-6 hover:bg-blue-800 font-bold text-white border border-blue-800 px-2 py-1 rounded">Sign Up</button>
-          </div>
+            <button className="px-5 py-2 bg-white hover:bg-gray-100 font-bold text-blue-600 text-sm border border-blue-900 rounded">Login</button>
+            <button className="px-5 py-2 bg-blue-700 hover:bg-blue-800 font-bold text-sm text-white border border-blue-800 rounded">Sign Up</button>
+          </div> 
       </div>  
-    </nav>
-
-    //Hero Section
-    
+    </nav>  
   );
 };
 
